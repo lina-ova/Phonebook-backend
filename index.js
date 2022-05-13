c6ba2151d9c5fc60127dceaf6bd1a0e08b2bbcf7
Polina Nazarova <polina.w.nazarova@helsinki.fi>
2022-05-13 15:41:57 +0300

    3.7-

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json())
 
@@ -84,7 +86,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
